895e23bc8aa7fba4c9acdac42d8ba8e5b497058e
Monika <budagova.monika5@yandex.ru>
2024-11-19 18:43:03 +0300

    update

---
+++ client/app.vue
@@ -0,0 +1,28 @@
+<template>
+    <NuxtLoadingIndicator throttle="0"/>
+    <Navbar />
+    <NuxtPage />
+    <Up />
+    <Footer />
+  </template>
+  
+  <script setup>
+    const base = 'http://localhost:1337' 
+    const api = await $fetch(`${base}/api/config?populate=*`)
+    const config = api.data
+  
+    useHead({
+      title: config.title,
+      meta: [
+        { name: 'description', content: config.desc },
+        { name: 'keywords', content: config.keywork },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: base+config.fanicon.url },
+      ],
+      bodyAttrs:{
+        class: 'container mx-auto bg-white dark:bg-gray-900'
+      }
+    })
+  </script>
+  
---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>    
+        <h2>Главная страница</h2>
+    </main>
+</template>
+<style scoped>
+main{
+    color: #000000;
+    background-color: #95B1EE;
+}
+</style>
