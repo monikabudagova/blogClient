6d220ad675de2fcb1e7e13cfd1fe694906dd0062
Monika <budagova.monika5@yandex.ru>
2024-12-05 20:22:01 +0300

    fix

--- client/pages/post/[id].vue
+++ client/pages/post/[id].vue
@@ -1,27 +1,50 @@
 <template>
-    <main>
-      <h2 class="text-4xl font-extrabold my-4 dark:text-white">{{ api.data.title }}</h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 mb-4">
-        <article v-for="post in displayedPosts" :key="post.id" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <NuxtLink class="block overflow-hidden" :to="'/post/' + post.documentId">
-                <img class="rounded-t-lg" :src="base_url+post.img.url" :alt=post.img.alternativeText />
-            </NuxtLink>
-            <div class="p-5">
-                <NuxtLink :to="'/post/' + post.documentId">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ post.title }}</h5>
-                </NuxtLink>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ post.desc }}</p>
-                <NuxtLink :to="'/post/' + post.documentId" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Подробнее
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </NuxtLink>
-            </div>
-        </article>
-      </div>
+<main class="w-full lg:px-40">
+        <!-- хлебные крошки -->
+        <nav class="flex" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li class="inline-flex items-center">
+                    <NuxtLink to="/blog"
+                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                        <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                        </svg>
+                        Блог
+                    </NuxtLink>
+                </li>
+                <li>
+                    <div class="flex items-center">
+                        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                        <NuxtLink :to="'/category/' + post.categories[0].documentId"
+                            class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                            {{ post.categories[0].title }}</NuxtLink>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                            <strong>{{ post.title }}</strong>
+                        </span>
+                    </div>
+                </li>
+            </ol>
+        </nav>
+        <div class="ns_post">
+            <div class="h-80 rounded-2xl my-4 bg-fixed bg-[length:100%_600px]" :style="'background-image: url('+base_url+post.img.url+')'"></div>
+            <h1 class="relative text-4xl">{{ post.title }} <Share /></h1>
+            <p class="opacity-40">{{ formatDate(post.publishedAt.substring(0, 10)) }} • {{ post.views }} просмотров</p>
+            <div v-html="mark"></div>
+        </div>
     </main>
-  
   </template>
   
   
@@ -29,5 +52,5 @@
     const base_url = "https://4ccc2241e457.vps.myjino.ru"
     const { id } = useRoute().params
     const api = await $fetch(`https://4ccc2241e457.vps.myjino.ru/api/posts/${id}?populate=*`)
-    const posts = api.data.posts
+    const post = api.data
   </script>
