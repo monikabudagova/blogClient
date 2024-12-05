9e741867788a8b583319ef837f4db8b5c778f131
Monika <budagova.monika5@yandex.ru>
2024-12-05 19:51:48 +0300

    up

--- client/pages/search.vue
+++ client/pages/search.vue
@@ -1,9 +1,9 @@
-<template>
-    <!-- <h1>поиск</h1>
+<!-- <template>
+     <h1>поиск</h1>
 </template>
 <script setup>
     const stringSearch = ref('')
     const response = await $fetch(`https://4ccc2241e457.vps.myjino.ru/api/posts?filters[$or][0][title]
     [$containsi]=${stringSearch}&filters[$or][1][body][$containsi]=${stringSearch}`)
     const search = response.data -->
-</script>
+</script> -->
