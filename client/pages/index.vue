2c556b1e7fa93109b0fc3fca8fa91b2c17ed8a90
monikabudagova <budagova.monika5@yandex.ru>
2024-12-06 22:44:27 +0300

    up

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
     <main>    
-        <h2>Главная страница</h2>
+        <h2>новости этой недели</h2>
     </main>
 </template>
 <style scoped>
 main{
     color: #000000;
     background-color: #95B1EE;
+}
+h2{
+    display: flex;
+    justify-content: center;
 }
 </style>
