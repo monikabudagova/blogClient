51b71c798c9f5ab652330e6dde906d05656dcc17
monikabudagova <budagova.monika5@yandex.ru>
2024-12-06 22:21:42 +0300

    fufu

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
+    "./node_modules/flowbite/**/*.js",
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./app.vue",
     "./error.vue",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
