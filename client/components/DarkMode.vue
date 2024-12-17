a35c48dcf853294f79f3b2d3ac5dd61706049e57
monikabudagova <budagova.monika5@yandex.ru>
2024-12-17 20:32:44 +0300

    update

--- client/components/DarkMode.vue
+++ client/components/DarkMode.vue
@@ -1,49 +1,51 @@
 <template>
-    <!-- темная тема -->
-    <button @click="toggleTheme()" id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-        <svg v-if="!isDark" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <button @click="toggleTheme" id="theme-toggle" type="button"
+        class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+        <svg v-if="isDark" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
         </svg>
         <svg v-else class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
+            <path
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                fill-rule="evenodd" clip-rule="evenodd"></path>
         </svg>
     </button>
 </template>
 
-
 <script setup>
-function toggleTheme (){
-    console.log('fefef');
-    
-}
-// const isDarkMode = ref('') // класс для HTML
-// const isDark = ref(false) // по умолчанию светлая тема
+const isDark = ref(false);
+const classDark = ref('')
 
-// onMounted(() => {
-//     if (localStorage.getItem('color-theme') === 'dark') {
-//         isDark.value = true;
-//         isDarkMode.value = 'dark'
-//     } else {
-//         isDark.value = false;
-//         isDarkMode.value = ''
-//     }
-// });
+// Проверяем локальное хранилище при монтировании
+onMounted(() => {
+    const storedTheme = localStorage.getItem('color-theme');
+    if (storedTheme === 'dark') {
+        isDark.value = true;
+        classDark.value = 'dark';
+    } else {
+        isDark.value = false;
+        classDark.value = '';
+    }  
+});
 
-// // Функция для переключения темы
+// Функция для переключения темы
 const toggleTheme = () => {
     isDark.value = !isDark.value;
+
     if (isDark.value) {
-        document.documentElement.classList.add('dark');
         localStorage.setItem('color-theme', 'dark');
-    } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
+        isDark.value = true;
+        classDark.value = 'dark';
+    } else if (!isDark.value) {
+        localStorage.removeItem('color-theme')
+        isDark.value = false;
+        classDark.value = '';
     }
 };
 
-useHead({
+useHeadSafe({
     htmlAttrs: {
-        class: isDarkMode.value
+        class: classDark,
     }
-})
+});
 </script>
