88ece660e61938ecffe283e2fdc90812d82b7deb
Monika <budagova.monika5@yandex.ru>
2025-01-16 20:53:13 +0300

    *

--- client/app.vue
+++ client/app.vue
@@ -10,6 +10,9 @@
     const base = 'https://4ccc2241e457.vps.myjino.ru' 
     const api = await $fetch(`${base}/api/config?populate=*`)
     const config = api.data
+
+    const isDarkMode = ref('')
+    const isDark = ref(false)
   
     useHead({
     title: config.title,
@@ -18,16 +21,46 @@
       { name: 'keywords', content: config.keywords },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: base+config.fanicon[0].url },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: base+config.favicon.url
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css',
+      },
     ],
+    htmlAttrs: {
+        class: isDarkMode.value
+    },
     bodyAttrs: {
       class: 'container mx-auto bg-white dark:bg-gray-900'
     },
     head: {
       script: [
-        { src: 'https://yastatic.net/share2/share.js', async: true, defer: true }
+        {
+          src: 'https://yastatic.net/share2/share.js',
+          async: true,
+          defer: true
+        },
+        {
+          src: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js',
+          async: true,
+          defer: true
+        }
       ]
     }
   })
+
+onMounted(() => {
+  if (localStorage.getItem('color-theme') === 'dark') {
+      isDark.value = true;
+      isDarkMode.value = 'dark'
+  } else {
+      isDark.value = false;
+      isDarkMode.value = ''
+  }
+});
   </script>
   
