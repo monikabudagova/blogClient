99ccbb7c1b43ac7376e58a90c34c5a5dace6a111
monikabudagova <budagova.monika5@yandex.ru>
2025-01-25 23:07:47 +0300

    меню готова

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,119 +1,113 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <!-- темная тема -->
-        <button @click="isDarkMode = isDarkMode == '' ? 'dark' : ''" class="bg-white-100">🌙</button>
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="http://localhost:3000/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://4ccc2241e457.vps.myjino.ru/uploads/free_icon_music_note_1816908_7084764faf.svg" class="h-10" alt="" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">music</span>
-    </a>
-    <div class="flex md:order-2">
-        <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-        </svg>
-        <span class="sr-only">Search</span>
-        </button>
-        <div class="relative hidden md:block">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-            <span class="sr-only">Search icon</span>
-        </div>
-        <input type="search" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        v-model="search.searchQuery" @keyup.enter="goToSearch" @focus="goToSearch" placeholder="поиск...">
-        </div> 
-        <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button>
-    </div>
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-        <div class="relative mt-3 md:hidden">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
+    <header>
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+            <!-- отвечает за расположение навигации -->
+            <div class="navigHead max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+                <NuxtLink to="/">
+                    <a href="http://localhost:3000/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="https://4ccc2241e457.vps.myjino.ru/uploads/free_icon_music_note_1816908_7084764faf.svg" class="h-10" alt="" />
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">music</span>
+                    </a>
+                </NuxtLink>
+
+                <div class="flex gap-8 md:order-2">
+                    <DarkMode />
+                    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+                    aria-expanded="false"
+                    class="hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                    <span class="sr-only">Search</span>
+                </button>
+                    <div class="relative hidden md:block">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                            <span class="sr-only">Search icon</span>
+                        </div>
+                        <input v-model="search.searchQuery" @focus="goToSearch" type="text" id="search-navbar"
+                            class="block w-full p-2 ps-10 md:ps-1 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Поиск...">
+                    </div>
+                    <button data-collapse-toggle="navbar-search" type="button"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-search" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="items-center justify-between hidden w-full lg:flex md:w-auto md:order-1" id="navbar-search">
+                    <div class="relative mt-3 md:hidden ">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <input v-model="search.searchQuery" @focus="goToSearch" type="text" id="search-navbar"
+                            class="block w-full p-2 ps-10 text-sm text-gray-900 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Поиск...">
+                    </div>
+                    <ul class="flex flex-col p-5 mt-4 font-bold md:space-x-8 md:flex-row md:mt-0 sm:flex-row sm:justify-center sm:mt-0 dark:border-gray-700">
+                        <li>
+                            <NuxtLink to="/"
+                                class="block py-2 px-5 text-blue-500 rounded hover:bg-gray-100 sm:text-sm md:hover:bg-transparent md:hover:text-purple-500 md:p-0 dark:text-blue-400 md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                :class="{ 'md:text-blue-500': isActive(''), 'dark:text-blue-700': isActive('') }">Главная
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/blog"
+                                class="block py-2 px-5 text-blue-500 rounded hover:bg-gray-100 sm:text-sm md:hover:bg-transparent md:hover:text-purple-500 md:p-0 md:dark:hover:text-purple-500 dark:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                :class="{ 'md:text-blue-500': isActive('blog') || isActive('post') || isActive('category'), 'dark:text-blue-700': isActive('blog') || isActive('post') || isActive('category') }">
+                                Новости
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/contact"
+                                class="block py-2 px-5 text-blue-500 rounded hover:bg-gray-100 sm:text-sm md:hover:bg-transparent md:hover:text-purple-500 md:p-0 dark:text-blue-400 md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                :class="{ 'md:text-blue-500': isActive('contact'), 'dark:text-blue-700': isActive('contact') }">
+                                Контакты
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <input type="search" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            v-model="search.searchQuery" @keyup.enter="goToSearch" @focus="goToSearch" placeholder="поиск...">
-        </div>
-        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <NuxtLink to="/"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-violet-500 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                        :class="{ 'md:text-blue-500': isActive('') }">
-                        Главная</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/blog"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-violet-500 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                        :class="{ 'md:text-blue-500': isActive('blog') || isActive('post') || isActive('gategory') }">Блог</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/contact"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-violet-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                        :class="{ 'md:text-blue-500': isActive('contact') }" >
-                        Контакты</NuxtLink>
-                </li>
-            </ul>
-        </div>
-    </div>
-    </nav>
+        </nav>
+    </header>
 </template>
+
 <script setup>
+import { useSearchStore } from '~/stores/search'
+
+const search = useSearchStore()
 const route = useRoute()
 const isActive = (path) => route.path.split('/')[1] === path
-const isDarkMode = ref('')
+
+const router = useRouter()
+function goToSearch() {
+    router.push({ path: '/search' })
+
+}
 
 useHead({
-    htmlAttrs: {
-        class: isDarkMode
-    },
     script: [
         {
-            src:'https://cdn.jsdevir.net/npm/flowbite@2.5.2/dist/flowbite.min.js',
+            src: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js',
             async: true,
             defer: true
         }
     ]
 })
-
-// поиск на сайте
-const search = useSearchStore()
-const router = useRouter()
-const inputSearch = ref(search.searchQuery)
-
-watch(() => search.searchQuery, (newQuery) => {
-    if (newQuery) router.push('/search')
-});
-
-const goToSearch = () => router.push('/search') 
 </script>
-
-<style scoped>
-    button{
-    float: right ;
-    padding: 10px;
-    }
-    ul {
-        
-        height: 50px;
-        width: max;
-        justify-content: space-evenly;
-
-        font-weight: bold;
-
-
-        list-style: none;
-        display: flex;
-        gap: 10px;
-        padding: 0;
-    }
-    li{
-        text-decoration: none;
-    }
-</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <main>
-    
+      
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Regain <mark class="px-2 text-white bg-blue-600 rounded-sm dark:bg-blue-500">control</mark> over your days</h1>
+    <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+
     <div id="parallax-world-of-ugg" class="bg-white text-black dark:bg-black dark:text-white">
       <section>
         <div class="title">
